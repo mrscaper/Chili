8562fd593b88a61d90b905ef9d0d737559503a32
botond <mr.scaper@hotmail.com>
2016-07-28 18:29:37 +0200

    sorting done

--- scripts/chili.js
+++ scripts/chili.js
@@ -1,48 +1,30 @@
+// hack for alphabetical sorting
+String.prototype.removeAccents = function(){
+    return this
+        .toLowerCase()
+        .replace(/[á]/gi,"a")
+        .replace(/[é]/gi,"e")
+        .replace(/[é]/gi,"i")
+        .replace(/[óöő]/gi,"o")
+        .replace(/[úüű]/gi, "u")
+        .replace(/[^a-zA-Z0-9]/g," ");
+}
+
 // init Isotope
 var $grid = $('.grid').isotope({
     itemSelector: '.product',
     layoutMode: 'fitRows',
     getSortData: {
-        name: '.name',
+        name: function( itemElem ) {
+            var name = $( itemElem ).find('.name').text();
+            return name.removeAccents();
+        },
         heat: '.heat parseInt'
     }
 });
 
-// filter functions
-var filterFns = {
-    // show if number is greater than 50
-    heatGreaterThan2: function() {
-        var heat = $(this).find('.heat').text();
-        return parseInt( heat, 10 ) > 2;
-    },
-    // show if name ends with -ium
-    ium: function() {
-        var name = $(this).find('.name').text();
-        return name.match( /ium$/ );
-    }
-};
-
-// bind filter button click
-$('#filters').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $grid.isotope({ filter: filterValue });
-});
-
-// bind sort button click
-$('#sorts').on( 'click', 'button', function() {
+$('.sort-by-button-group').on( 'click', 'button', function() {
     var sortByValue = $(this).attr('data-sort-by');
-    $grid.isotope({ sortBy: sortByValue });
+    var sortByAsc= ($(this).attr('data-sort-asc') == "true");
+    $grid.isotope({ sortBy: sortByValue, sortAscending:sortByAsc });
 });
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $( this ).addClass('is-checked');
-    });
-});
-
-
