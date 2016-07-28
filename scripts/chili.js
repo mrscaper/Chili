b3e17c36963918c612695277891c08d4e2ffa6cd
botond <mr.scaper@hotmail.com>
2016-07-28 21:38:16 +0200

    heat slider

--- scripts/chili.js
+++ scripts/chili.js
@@ -8,7 +8,7 @@ String.prototype.removeAccents = function(){
         .replace(/[óöő]/gi,"o")
         .replace(/[úüű]/gi, "u")
         .replace(/[^a-zA-Z0-9]/g," ");
-}
+};
 
 // init Isotope
 var $grid = $('.grid').isotope({
@@ -27,4 +27,25 @@ $('.sort-by-button-group').on( 'click', 'button', function() {
     var sortByValue = $(this).attr('data-sort-by');
     var sortByAsc= ($(this).attr('data-sort-asc') == "true");
     $grid.isotope({ sortBy: sortByValue, sortAscending:sortByAsc });
+});
+
+function updateFilter()
+{
+    var number = $(this).find('.heat').text();
+    var parsedNumber =parseInt( number, 10 );
+    return (parsedNumber >= ui.values[0]) && (parsedNumber <= ui.values[1]);
+}
+
+$( "#heat-slider" ).slider({
+    range: true,
+    min: 1,
+    max: 5,
+    values: [ 1, 5 ],
+    slide: function( event, ui ) {
+        $grid.isotope({filter: function() {
+            var number = $(this).find('.heat').text();
+            var parsedNumber =parseInt( number, 10 );
+            return (parsedNumber >= ui.values[0]) && (parsedNumber <= ui.values[1]);
+        }})
+    }
 });
